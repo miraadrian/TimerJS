2fda4f0e5cc53b6a85ea1ea5c18b8cfe13d05ac4
Adrian Mira <mira.adrian@outlook.com>
2025-07-25 17:21:10 -0300

    Realizado a lógica com setInterval, clearInterval e ajeitado css

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+function criaHoradosSegundos(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+let segundos = 0;
+let timer;
+
+iniciar.addEventListener('click', function(evento){
+    clearInterval(timer);
+    relogio.style.color = 'black';
+    iniciaRelogio();
+
+})
+
+
+zerar.addEventListener('click', function(evento){
+   zeraRelogio();
+})
+
+pausar.addEventListener('click', function(evento){
+    pausaRelogio();
+})
+
+function iniciaRelogio(){
+    timer = setInterval(function(){
+        segundos++;
+        relogio.innerHTML = criaHoradosSegundos(segundos);
+    }, 1000)
+}
+
+function pausaRelogio(){
+    clearInterval(timer);
+    relogio.style.color = 'red';
+}
+
+function zeraRelogio(){
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+    timer = 0;
+    relogio.style.color = 'black';
+};
